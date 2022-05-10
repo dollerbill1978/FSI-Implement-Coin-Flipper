ab7670d039f70367dda2e071a8d01e62f69eaa7e
Jesse Oller <dollerbill1978@yahoo.com>
2022-05-09 22:10:21 -0500

    add new file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,71 @@
 // TODO: Declare any global variables we need
+    let flip;
+    let totalFlips = 0;
+    let numHeads = 0;
+    let numTails = 0;
+    let headPerc = 0;
+    let tailPerc = 0;
+    let flipButton = document.querySelector("#flip");
+    let resetButton = document.querySelector("#clear");
+    let numHeadsBox = document.querySelector("#heads");
+    let percHeadsBox = document.querySelector("#heads-percent");
+    let numTailsBox = document.querySelector("#tails");
+    let percTailsBox = document.querySelector("#tails-percent");
+    let pennyImage = document.querySelector("#pennyimage");
 
 
 document.addEventListener('DOMContentLoaded', function () {
     // This is just a sanity check to make sure your JavaScript script is getting loaded
     // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
+    console.log("hi")
+   
+
 
     // TODO: Add event listener and handler for flip and clear buttons
+    flipButton.addEventListener("click", flipper);
+    function flipper (){
+   
+        flip = Math.round(Math.random());
+        //heads is 0
+        if (flip === 0) {
+            totalFlips++;
+            numHeads++;
+            headPerc = Math.round((numHeads/totalFlips)*100);
+            tailPerc = Math.round((numTails/totalFlips)*100);
+            numHeadsBox.textContent = numHeads;
+            percHeadsBox.textContent = headPerc;
+            percTailsBox.textContent = tailPerc;
+            numTailsBox.textContent = numTails;
+            pennyImage.src = "assets\\images\\penny-heads.jpg"; 
+            
+        } else {
+            totalFlips++;
+            numTails++;
+            headPerc = Math.round(numHeads/totalFlips*100);
+            tailPerc = Math.round(numTails/totalFlips*100);
+            numHeadsBox.textContent = numHeads;
+            percHeadsBox.textContent = headPerc;
+            percTailsBox.textContent = tailPerc;
+            numTailsBox.textContent = numTails;  
+            pennyImage.src = "assets\\images\\penny-tails.jpg";
+
+        }   
+
+    }
+    
+    resetButton.addEventListener("click", reset);
+    function reset (){
+        totalFlips = 0;
+        numTails =0;
+        headPerc = 0;
+        tailPerc = 0;
+        numHeadsBox.textContent = 0;
+        percHeadsBox.textContent = 0;
+        percTailsBox.textContent = 0;
+        numTailsBox.textContent = 0;  
+    }
+
+
 
     // Flip Button Click Handler
         // TODO: Determine flip outcome
@@ -24,4 +83,4 @@ document.addEventListener('DOMContentLoaded', function () {
         // TODO: Reset global variables to 0
         // TODO: Update the scoreboard (same logic as in flip button click handler)
 
-})
+});
